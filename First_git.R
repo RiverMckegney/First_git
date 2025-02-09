f1d5ffb03ea779faae450c1600b15bb9da0b6fb4
RiverMckegney <rmckegneycsu@gmail.com>
2025-02-09 11:11:41 -0800

    Adding first git Rscript.

---
+++ First_git.R
@@ -0,0 +1,11 @@
+
+# Verify Connection:
+usethis::git_sitrep()
+
+# First, lets explain the code above: package::function()
+
+# Note: 'usethis' is the package, while :: (double colon) is extracting specific function [git_sitrep()] from that package. 
+# So instead of library(usethis), this method accesses package and uses it for the specific function without needing to load the entire package.
+# Note: This might be important later on, when packages have conflicting function styles (and recognizing the order packages are loaded matters).
+
+
